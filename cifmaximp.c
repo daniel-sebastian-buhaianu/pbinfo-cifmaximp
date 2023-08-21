aa2ff6a2899032e02bb19e3e09a1edc0fbc0dedf
Dan Web <128343993+therealdanweb@users.noreply.github.com>
2023-08-21 13:38:18 +0300

    sol2

--- cifmaximp.c
+++ cifmaximp.c
@@ -2,32 +2,27 @@
 
 int main()
 {
-    unsigned short n, i = 0, cif_max_imp = 0;
-    char c, cif_max = '0', ok;
+	unsigned short n, i, cif_max, cif, cifmaximp = 0;
+	int nr;
 
-    scanf("%hu", &n);
+	scanf("%hu", &n);
+	
+	for (i = 0; i < n; i++) {
+		scanf("%d", &nr);
+        
+		cif_max = 0;
+		while (nr > 0) {
+			cif = nr % 10;
+            
+			if (cif > cif_max) cif_max = cif;
+            
+			nr /= 10;
+		}
+        
+		if (cif_max % 2) cifmaximp++;
+	}
 
-    while (i < n) {
-        scanf("%c", &c);
-        ok = 0;
-        while (c >= '0' && c <= '9') {
-            ok = 1;
+	printf("%hu", cifmaximp);
 
-            if (cif_max < c) cif_max = c;
-
-            scanf("%c", &c);
-        }
-
-        if (ok) {
-            if ((cif_max - '0') % 2) cif_max_imp++;
-
-            i++;
-
-            cif_max = '0';
-        }
-    }
-
-    printf("%hu", cif_max_imp); 
-
-    return 0;
+	return 0;
 }
